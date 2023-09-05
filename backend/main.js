96ff7c441e2f9fdbfc581830fa0ab06d71b5116d
Tahmidur_Rabb <tr1476@nyu.edu>
2023-09-04 22:01:14 -0400

    changes

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,6 @@
 //Setting authentification 
+//Node and express
+
+const express = require('express'); // Load the 'express' module from npm
+const myModule = require('./myModule'); // Load a custom module using a relative path
+
